d217821b9bf1a81564eea1d2959bb7c227ceadd6
Vinh Hoang <vinhhn3@fe.edu.vn>
2023-11-28 10:10:12 +0700

    add skeleton

---
+++ src/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-primary">
+      <h2>TCD0701 React Origami</h2>
+      <ul>
+        <li>
+          <a href="#">Login</a>
+        </li>
+        <li>
+          <a href="#">Register</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
